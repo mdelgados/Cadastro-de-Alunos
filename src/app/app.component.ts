9555fd8d3f9e05064b5557cf42d176502fc9033c
mdelgados <mluisdelgadosantos@gmail.com>
2025-12-17 16:58:49 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Aluno {
+  nome: string;
+  nota1: number | null;
+  nota2: number | null;
+  media: number;
+  condicao: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'desafio-final';
+
+  nome = '';
+  nota1: number | null = null;
+  nota2: number | null = null;
+
+  alunos: Aluno[] = [];
+  editandoIndex: number | null = null;
+
+  salvar() {
+    if (!this.nome || this.nota1 === null || this.nota2 === null) {
+      return;
+    }
+
+    const media = (this.nota1 + this.nota2) / 2;
+    const condicao = media >= 7 ? 'Aprovado' : 'Reprovado';
+
+    const aluno: Aluno = {
+      nome: this.nome,
+      nota1: this.nota1,
+      nota2: this.nota2,
+      media,
+      condicao
+    };
+
+    if (this.editandoIndex !== null) {
+      this.alunos[this.editandoIndex] = aluno;
+      this.editandoIndex = null;
+    } else {
+      this.alunos.push(aluno);
+    }
+
+    this.limparFormulario();
+  }
+
+  editar(index: number) {
+    const aluno = this.alunos[index];
+    this.nome = aluno.nome;
+    this.nota1 = aluno.nota1;
+    this.nota2 = aluno.nota2;
+    this.editandoIndex = index;
+  }
+
+  excluir(index: number) {
+    this.alunos.splice(index, 1);
+  }
+
+  limparFormulario() {
+    this.nome = '';
+    this.nota1 = null;
+    this.nota2 = null;
+  }
 }
